5d462dcba0eb1c3a553cc07f83ea0814b5ba890b
AnittaMaryJose <harineduvely2015@gmail.com>
2023-12-20 10:59:38 +0530

    update username

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from .serializers import ItemSerializer
 
 
 # Create your views here.
+
 class ItemListCreateView(ListCreateAPIView):
     queryset =Item.objects.all()
     serializer_class = ItemSerializer
