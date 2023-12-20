8fb74856d485373543f84ca2cb01431c74df98a8
Harikrishnan NJ <harineduvely2015@gmail.com>
2023-12-20 10:22:59 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from . models import Item
+from .serializers import ItemSerializer
+
+
+# Create your views here.
+class ItemListCreateView(ListCreateAPIView):
+    queryset =Item.objects.all()
+    serializer_class = ItemSerializer
