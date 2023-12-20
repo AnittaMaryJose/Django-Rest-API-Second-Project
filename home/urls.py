75d930c941e324c29c97710e8f3d4b74afa38648
AnittaMaryJose <anittamaryjose25@gmail.com>
2023-12-20 11:35:45 +0530

    changes

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . views import ItemListCreateView
+from . views import ItemListCreateView,ItemDetailAPIView
 
 urlpatterns = [
     path('', ItemListCreateView.as_view(),name='listcreate'), 
+    path('items/<pk>/', ItemDetailAPIView.as_view(), name='list'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView
 from . models import Item
 from .serializers import ItemSerializer
-
-
-
-
-
-
+from rest_framework.generics import ListCreateAPIView,ListAPIView,RetrieveUpdateDestroyAPIView
 
 # Create your views here.
 
 class ItemListCreateView(ListCreateAPIView):
     queryset =Item.objects.all()
+    serializer_class = ItemSerializer
+class ItemDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
     serializer_class = ItemSerializer
